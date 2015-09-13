0be393f868cbf30d87e28cf33fcd979577848662
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-09-13 20:27:29 +0300

    problem 11 solved

--- ProjectEuler+/11/largest_product.rb
+++ ProjectEuler+/11/largest_product.rb
@@ -1,18 +1,41 @@
 #!/usr/bin/env ruby
 
-require 'pp'
-
+data = []
 lines = 20
-matrix = []
 while lines > 0
   row = $stdin.gets.strip.split(' ').map(&:to_i)
-  matrix << row
+  data << row
   lines -= 1
 end
 
-pp matrix
+@max = 0
+module Problem011
+ 
+  def horizontal ary
+    ary.each do |row|
+      row.each_cons(4) do |con|
+        r = con.inject(&:*)
+        @max = r if r > @max
+      end
+    end
+  end
 
-#TODO
-#find solution
+  def slash ary
+    (0..16).each do |n|
+      (0..16).each do |m|
+        r = (0..3).inject(1){|p, i| p * ary[n + i][m + i]}
+        @max = r if r > @max
+      end
+    end
+  end
+end
+include Problem011
+horizontal data
+horizontal data.transpose
+slash data
+slash data.map(&:reverse).transpose
+p @max
+
+
 
 
